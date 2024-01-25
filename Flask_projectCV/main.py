de0991a3e46c1787b23f2b7549f9f1be5d9f7828
luisbranco00 <94049232+luisbranco00@users.noreply.github.com>
2024-01-25 10:39:19 +0000

    Update main.py

--- Flask_projectCV/main.py
+++ Flask_projectCV/main.py
@@ -11,31 +11,32 @@ def submit():
     email = request.form.get('Email')
     company = request.form.get('Company')
     propose = request.form.get('Propose')
-    wage = request.form.get('Wage')
 
     # Store data (replace this with your storage logic)
-    print(f"Received: Name - {name}, Email - {email}, Company - {company}, Propose - {propose}, Wage - {wage}")
-
+    print(f"Received: Name - {name}, Email - {email}, Company - {company}, Propose - {propose}")
+    form_data = (f"Name: {name}\nEmail: {email}\nCompany: {company}\nPropose: {propose}\n")
+    with open('form_response.txt', 'a') as file:
+        file.write(form_data + '\n\n')
     return "Form submitted successfully!", 302, {'Location': url_for('home')}
- 
-@app.route("/")  
+
+@app.route("/")
 def home():
     return render_template('home.html',methods = ['GET', 'POST'])
 
 @app.route('/form')
 def form():
     return render_template('form.html')
-@app.route("/scholar_projects")
-def scholar_projects():
+@app.route("/scholar_project")
+def scholar_project():
     return render_template('scholar_projects.html',methods = ['GET', 'POST'])
 
 
 @app.route("/about_me")
 def about_me():
     return render_template('about_me.html',methods = ['GET', 'POST'])
-    
 
 
-    
+
+
 if __name__ == "__main__":
     app.run()
